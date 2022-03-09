27d21d04cd7275643f87de619018ff1d9aa9cdc4
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-03-09 22:30:07 +0530

    Update Trailing Zeros in Factorial.cpp

--- Trailing Zeros in Factorial.cpp
+++ Trailing Zeros in Factorial.cpp
@@ -1,6 +1,18 @@
-int Solution::trailingZeroes(int A){
+/*
+int Solution::trailingZeroes(int A) {
     int c=0;
     for(int i=5;A/i>=1;i=i*5)
         c=c+(A/i);
     return c;
+}
+*/
+int Solution::trailingZeroes(int A) {
+    int i=5;
+    int ans = 0;
+    while(A>=i){
+        int x = A/i ;
+        ans+=x;
+        i*=5;
+    }
+    return ans;
 }
