d9eb891e82149a53f5311a57a64ad7a9d105506f
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-03-21 20:18:18 +0530

    Update Move Zeroes.cpp

--- Move Zeroes.cpp
+++ Move Zeroes.cpp
@@ -1,4 +1,14 @@
-vector<int> Solution::solve(vector<int> &A) {
+vector<int> Solution::solve(vector<int> &nums) {
+        int j = 0;
+        // move all the nonzero elements advance
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] != 0)
+                nums[j++] = nums[i];
+        for (;j < nums.size(); j++)
+            nums[j] = 0;
+        return nums;
+}
+/*vector<int> Solution::solve(vector<int> &A) {
     int low=0,high=0;
     while(high!=A.size()-1){
         while(A[low]!=0 && low!=A.size()-1)
@@ -11,3 +21,4 @@ vector<int> Solution::solve(vector<int> &A) {
     }
     return A;
 }
+*/
