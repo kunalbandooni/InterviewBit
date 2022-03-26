56cf248b4920942d7a24c8f3821107c108d36574
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-03-26 07:48:00 +0530

    Update Pick from both sides!.cpp

--- Pick from both sides!.cpp
+++ Pick from both sides!.cpp
@@ -15,3 +15,27 @@ int Solution::solve(vector<int> &A, int B) {
     }
     return ans;
 }
+
+// Reviewed someone's code, putting it here :D
+/*
+Try not to make a simple problem complex.
+
+The problem statement says, "You can pick B elements from 
+either left or right end of the array A to get maximum sum." 
+So, middle elements won't ever be even seen by our 
+traversing. All we need are the first and last 'B' elements 
+(as we can pick from only left or right end of array).
+
+What you can do is, first of all, check the sum for the 
+first 'k' elements.
+
+This is for cases like : 
+A: [1,-20,1000,0,1,2,2,3,1,2] 
+B: 3
+
+Then, you can simply use the sliding window approach, by
+adding the last elements and subtracting the first elements 
+from the array.
+
+Also, keep checking for maximum sum.
+*/
