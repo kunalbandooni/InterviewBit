48c715b347b72119b797d8d00695733752257071
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-04-07 23:20:05 +0530

    Create Level Order.cpp

---
+++ Level Order.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+void build(TreeNode* root,vector<vector<int>> &ret,int depth){
+    if(!root)
+        return;
+    if(ret.size()==depth)
+        ret.push_back(vector<int>(0));
+    ret[depth].push_back(root->val);
+    build(root->left,ret,depth+1);
+    build(root->right,ret,depth+1);
+}
+vector<vector<int> > Solution::levelOrder(TreeNode* B) {
+    vector<vector<int>> ret;
+    build(B,ret,0);
+    return ret;
+}
