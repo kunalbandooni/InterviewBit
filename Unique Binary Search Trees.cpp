33774163108ed19c7a6a5949575f8d3aa2011201
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-04-09 23:20:43 +0530

    Update Unique Binary Search Trees.cpp

--- Unique Binary Search Trees.cpp
+++ Unique Binary Search Trees.cpp
@@ -7,6 +7,38 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+vector<TreeNode*> helper(int l, int r, vector<vector<vector<TreeNode*>>> &dp) {
+    if (l > r)
+        return vector<TreeNode*>{NULL};
+    if (dp[l][r].size() != 0)
+        return dp[l][r];
+    vector<TreeNode*> local;
+    for (int i = l; i <= r; i++) {
+        // get all trees of left sub array
+        vector<TreeNode*> left_local = helper(l, i - 1, dp);
+        // get all trees of right sub array
+        vector<TreeNode*> right_local = helper(i + 1, r, dp);
+        for (auto const &ll: left_local) {
+            for (auto const &rr: right_local) {
+                // make current element the root for all left and right trees
+                TreeNode* root = new TreeNode(i);
+                root->left = ll;
+                root->right = rr;
+                local.push_back(root);
+            }
+        }
+    }
+    dp[l][r] = local;
+    return dp[l][r];
+}
+vector<TreeNode*> Solution::generateTrees(int A) {
+    vector<vector<vector<TreeNode*>>> dp(A + 1, vector<vector<TreeNode*>>(A + 1, vector<TreeNode*>(0)));
+    for (int i = 1; i <= A; i++)
+        dp[i][i] = vector<TreeNode*>{new TreeNode(i)};
+    return helper(1, A, dp);
+}
+
+/*
 vector<TreeNode*> generateTree(int st, int end){
     if(st>end){
         vector<TreeNode*> ans;
@@ -31,3 +63,4 @@ vector<TreeNode*> generateTree(int st, int end){
 vector<TreeNode*> Solution::generateTrees(int A) {
     return generateTree(1,A);
 }
+*/
