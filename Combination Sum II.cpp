4c05037ae56e74fde86c8734a616dba57f0f3535
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-04-21 18:04:07 +0530

    Create Combination Sum II.cpp

---
+++ Combination Sum II.cpp
@@ -0,0 +1,28 @@
+void util(vector<vector<int>> &res, vector<int> v, vector<int> &A, int T, int sum, int n){
+    if(sum>T || n<0)
+        return;
+    if(sum==T){
+        sort(v.begin(),v.end());
+        if(find(res.begin(),res.end(),v)==res.end())
+            res.push_back(v);
+        return;
+    }
+    if(sum+A[n-1]<=T){
+        sum+=A[n-1];
+        v.push_back(A[n-1]);
+        util(res,v,A,T,sum,n-1);
+        v.pop_back();
+        sum-=A[n-1];
+        util(res,v,A,T,sum,n-1);
+    }
+    else
+        util(res,v,A,T,sum,n-1);
+    return;
+}
+vector<vector<int>> Solution::combinationSum(vector<int> &A, int B){
+    vector<vector<int>>res;
+    vector<int> v;
+    util(res,v,A,B,0,A.size());
+    sort(res.begin(),res.end());
+    return res;
+}
