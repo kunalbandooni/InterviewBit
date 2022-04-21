58f821501e85d258f047193df307a78355f973ab
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-04-21 18:05:49 +0530

    Update Combination Sum II.cpp

--- Combination Sum II.cpp
+++ Combination Sum II.cpp
@@ -1,4 +1,36 @@
-void util(vector<vector<int>> &res, vector<int> v, vector<int> &A, int T, int sum, int n){
+void MakeAns(int curr,int target,vector<int>& path, vector<vector<int> >& ans,vector<int>& A)
+{
+    if(curr==A.size())
+    {
+        if(target==0)
+            ans.push_back(path);
+        return;
+    }
+    if(target<0)
+        return;
+    int val = A[curr], end = curr;
+    while(end<A.size() && A[end]==val)
+            end++;
+    int count = end - curr;
+    for(int i=0;i<=count;i++)
+    {
+        MakeAns(end,target,path,ans,A);
+        path.push_back(val);
+        target-=val;
+    }
+    
+    path.erase(path.begin()+path.size()-count-1,path.end());
+    return;
+}
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int target) {
+    sort(A.begin(),A.end());
+    vector<vector<int> > ans;
+    vector<int> path;
+    MakeAns(0,target,path,ans,A);
+    return ans;
+}
+
+/*void util(vector<vector<int>> &res, vector<int> v, vector<int> &A, int T, int sum, int n){
     if(sum>T || n<0)
         return;
     if(sum==T){
@@ -26,3 +58,4 @@ vector<vector<int>> Solution::combinationSum(vector<int> &A, int B){
     sort(res.begin(),res.end());
     return res;
 }
+*/
