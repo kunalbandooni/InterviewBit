dec62c21d97442efab0ab65152f8ad2790058b95
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-05-08 22:34:26 +0530

    Update Implement Power Function.cpp

--- Implement Power Function.cpp
+++ Implement Power Function.cpp
@@ -1,3 +1,5 @@
+/*
+// SIMPLER SOLUTION DOWN THERE :)
 #define ll long long int
 int Solution::pow(int x, int n, int d) {
     if(d == 1) 
@@ -17,5 +19,23 @@ int Solution::pow(int x, int n, int d) {
     if(ans < 0)
         ans = (d - abs(ans) % d);
     return ans;
-    
+}
+*/
+int Solution::pow(int x, int n, int d) {
+    if(n==1){
+        if(x<0)
+            return (x%d)+d;
+        return x;
+    }
+    if(n==0 and x==0)
+        return 0;
+    if(n==0)
+        return 1%d;
+    long long temp = pow(x,n/2,d)%d;
+    long long mul = (temp * temp)%d;
+    if(mul<0)
+        mul = mul + d;
+    if(n%2==0)
+        return mul;
+    return (mul*x)%d;
 }
