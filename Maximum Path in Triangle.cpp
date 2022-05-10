3ca2466fd5f3afe86a670cfc9b02d4339ae1b328
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-05-10 20:05:11 +0530

    Update Maximum Path in Triangle.cpp

--- Maximum Path in Triangle.cpp
+++ Maximum Path in Triangle.cpp
@@ -1,4 +1,4 @@
-int dp[1001][1001];
+/*int dp[1001][1001];
 int cal(int i,int j,vector<vector<int>> &A){
     if(i==A.size() || j==A.size())
         return 0;
@@ -9,4 +9,12 @@ int cal(int i,int j,vector<vector<int>> &A){
 int Solution::solve(vector<vector<int>> &A){
     memset(dp, -1 ,sizeof dp);
     return cal(0,0,A);  
+}
+*/
+int Solution::solve(vector<vector<int> > &a) {
+    int n=a.size();
+    for(int i=n-2;i>=0;i--)
+        for(int j=0;j<a[i].size();j++)
+            a[i][j]+=max(a[i+1][j],a[i+1][j+1]);
+    return a[0][0];
 }
