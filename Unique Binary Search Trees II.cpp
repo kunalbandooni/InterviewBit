8e278ca6dba2e7d85fddbeb900a176221da37c7f
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-06-14 09:17:59 +0530

    Update Unique Binary Search Trees II.cpp

--- Unique Binary Search Trees II.cpp
+++ Unique Binary Search Trees II.cpp
@@ -1,3 +1,15 @@
+/*
+This can be also done using Catalan Number Formula
+
+Series of numbers you will get after solving is called
+The Catalan series, and a lot of question are asked
+based on simply calculation of Nth catalan number. 
+
+Read more at:
+_ https://www.geeksforgeeks.org/applications-of-catalan-numbers _
+*/
+
+
 int Solution::numTrees(int A) {
     int dp[A+1] = {0};
     dp[0] = 1;
