fbfbf8204b752be02fd3d148b2d0dcee60269c06
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-07-20 10:53:12 +0530

    Create Remove Half Nodes.cpp

---
+++ Remove Half Nodes.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+TreeNode* Solution::solve(TreeNode* A) {
+    if(!A)
+        return nullptr;
+    if(A->left and A->right){
+        A->left = solve(A->left);
+        A->right = solve(A->right);
+    }
+    else if(A->left)
+        A = solve(A->left);
+    else if(A->right)
+        A = solve(A->right);
+    return A;
+}
