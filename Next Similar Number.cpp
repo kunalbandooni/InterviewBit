03276821795c6edd1c3afe02c645e5a63b07030b
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-08-28 11:28:30 +0530

    Create Next Similar Number.cpp

---
+++ Next Similar Number.cpp
@@ -0,0 +1,14 @@
+string Solution::solve(string nums) {
+    int i = nums.size() - 2;
+    while (i >= 0 && nums[i + 1] <= nums[i])
+        i--;
+    if (i >= 0) {
+        int j = nums.size() - 1;
+        while (nums[j] <= nums[i])
+            j--;
+        swap(nums[i], nums[j]);
+        reverse(nums.begin()+i+1,nums.end());
+        return nums;
+    }
+    return "-1";
+}
