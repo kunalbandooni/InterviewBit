09bdc47269d6d824911d475ba167f8b83c335933
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-09-04 15:49:34 +0530

    Update Path to Given Node.cpp

--- Path to Given Node.cpp
+++ Path to Given Node.cpp
@@ -7,6 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// Using two vector
+/*
 vector<int> g;
 void func(TreeNode* A, int B, vector<int> v){
     if(!A)   return;
@@ -20,11 +22,21 @@ void func(TreeNode* A, int B, vector<int> v){
     
     func(A->left,B,v);
     func(A->right,B,v);
+}*/
+
+// Using one array...
+bool func(TreeNode* root, int x, vector<int> &v){
+    if(!root)   return false;
+    v.push_back(root->val);
+    if(x == root->val)  
+        return true;
+    if(func(root->left, x, v) or func(root->right, x, v))
+        return true;
+    v.pop_back();
+    return false;
 }
 vector<int> Solution::solve(TreeNode* A, int B) {
     vector<int> v;
-    
     func(A,B,v);
-    
-    return g;
+    return v;
 }
