35396bf561c94a8fdf7d8eed952b280476126b40
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-09-30 21:11:57 +0530

    Create Double Increasing Series.cpp

---
+++ Double Increasing Series.cpp
@@ -0,0 +1,13 @@
+int mod = 1e9+7;
+int helper(int A, int len, vector<vector<int>> &dp){
+   if(len == 1) return A; // A = 5, => {1 || 2 || 3 || 4 || 5}
+   if(A == 0)   return 0; // A = 0 => no positives are present
+
+   if(dp[A][len] != -1) return dp[A][len];
+
+   return dp[A][len] = (helper(A/2, len-1, dp) + helper(A-1, len, dp))%mod;
+}
+int Solution::solve(int A, int len) {
+    vector<vector<int>> dp(A+1, vector<int>(len+1, -1));
+    return helper(A, len, dp);
+}
