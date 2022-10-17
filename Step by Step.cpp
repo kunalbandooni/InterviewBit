22071339bbea514cb8e8dd51192d6b380ca13b26
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-10-17 19:16:07 +0530

    Update Step by Step.cpp

--- Step by Step.cpp
+++ Step by Step.cpp
@@ -12,11 +12,12 @@ int Solution::solve(int A) {
     //Now afer this loop terminates we have 2 conditions
     //1. Our running sum is equal to A. Thats good we can return in i
     //2. Our running sum overshoot a
+    
     //Here is our strategy: We will keep on adding moves to the running sum till the time an overshooting value is not divisible by 2
     //That is beacuse everytime we flip the sign of x number, our sum should decrease by 2*x
-    while((running_sum-A)%2 != 0){
-        i++;
-        running_sum+=i;
-    }
-    return i;
+    if(running_sum == A || (running_sum - A)%2 == 0)
+        return i;
+    if(i%2 == 0)
+        return i+1;
+    return i+2;
 }
