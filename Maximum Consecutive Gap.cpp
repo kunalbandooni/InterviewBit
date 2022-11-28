cdca0bd75971768ec66e20f3322283f8e64168b7
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2022-11-28 17:33:14 +0530

    Create Maximum Consecutive Gap.cpp

---
+++ Maximum Consecutive Gap.cpp
@@ -0,0 +1,13 @@
+int Solution::maximumGap(const vector<int> &A) {
+    if(A.size() <= 2)   return 0;
+    vector <int> B;
+    for(int i=0;i<A.size();i++)
+        B.push_back(A[i]);
+    sort(B.begin(),B.end());
+    int maxi=0;
+    for(int i=0; i<B.size()-1 ;i++){
+        int diff = B[i+1] - B[i];
+        maxi = max(maxi, diff);
+    }
+    return maxi;
+}
