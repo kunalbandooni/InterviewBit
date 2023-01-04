82df1fba88db33aea43c43ac3c1aafa1105ec213
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2023-01-04 09:12:50 +0530

    Create Next Number.cpp

---
+++ Next Number.cpp
@@ -0,0 +1,23 @@
+What’s the next number in this sequence:
+
+10, 9, 60, 90, 70, 66, 96, 94, 98, ... ?
+  
+  
+  
+  
+  
+Solution:
+
+73 / 77
+  
+Ten
+Nine
+Sixty
+Ninety
+Seventy
+Sixty-six
+Ninety-six
+Ninety-four
+Ninety-eight
+They are in ascending order, based on the number of letters in the spelled-out numbers.
+A correct response will have twelve letters: 73 and 77 for instance.
