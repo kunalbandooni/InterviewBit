8fb87f34b3ecac6dc08c71b55e82f8d6deb5e839
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2023-01-13 14:23:03 +0530

    Create Gas Station.cpp

---
+++ Gas Station.cpp
@@ -0,0 +1,14 @@
+int Solution::canCompleteCircuit(const vector<int> &A, const vector<int> &B) {
+    int startIndex;
+    int n = A.size();
+    for(int i = 0; i<n; i++) {
+        startIndex = i;
+        int gasleft = A[i] -B[i];
+        while(gasleft >= 0 && (i - startIndex) < n) {
+            gasleft += A[++i%n]-B[i%n];
+        }
+        if(gasleft >= 0 )
+            return startIndex;
+    }// end for
+    return -1;
+}
